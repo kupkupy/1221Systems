fb9b45b70f34ec182f30d0394b303d8973109e5d
Tatyana <etaniks@mail.ru>
2023-08-17 12:41:47 +0300

    Initial Commit

---
+++ 1221SystemsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1221SystemsTest
+//
+//  Created by Tanya on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
